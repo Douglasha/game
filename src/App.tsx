7166b07f472d76fd0d4c25c0e9ca6529a79410b3
Douglasha <douglas.henriqueamaral@gmail.com>
2025-08-06 20:19:30 -0300

    Iniciando Projeto Jogo de Adivinhação

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import styles from "./App.module.css"
+
+import { Header } from "./components/Header"
+
+export default function App() {
+  function handleRestartGame() {
+    alert("Reiniciar o jogo")
+  }
+  return (
+    <div className={styles.container}>
+      <main>
+        <Header current={5} max={10} onRestart={handleRestartGame} />
+      </main>
+    </div>
+  )
+}
